966af2f2038a35e7031dd4ecd5f77c634ef89cb8
Vojtech Prokop <prokop.vojtech@gmail.com>
2022-02-24 18:53:05 +0100

    feat: idk

--- src/config.py
+++ src/config.py
@@ -1,1 +1,2 @@
-directory = f'C://Users//proko//Desktop//University//iv//aks//datasets'
+#directory = f'C://Users//proko//Desktop//University//iv//aks//datasets'
+directory = f"C://Users//Vojta//Desktop//iv//AKS//datasets"
